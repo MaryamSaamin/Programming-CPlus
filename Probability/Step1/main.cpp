e9460033c9f4e14f525603e5f0720d40954c6b63
MaryamSaamin <maryam.saamin@gmail.com>
2025-11-25 12:06:25 +0200

    Add files via upload

---
+++ Probability/Step1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "InverseCumulativeNormal.h"   // include your header
+
+int main() {
+    quant::InverseCumulativeNormal icn; // mean=0, sigma=1
+
+    double xs[] = {1e-12, 1e-6, 0.01, 0.1, 0.5, 0.9, 0.99, 1 - 1e-6, 1 - 1e-12};
+    for (double x : xs) {
+        double z = icn(x);
+        std::cout << "scalar  x=" << x << "  z=" << z << "\n";
+    }
+
+    const double xin[] = {0.0001, 0.01, 0.1, 0.3, 0.5, 0.7, 0.9, 0.99, 0.9999};
+    double zout[std::size(xin)];
+    icn(xin, zout, std::size(xin));
+
+    for (std::size_t i = 0; i < std::size(xin); ++i) {
+        std::cout << "vector  x=" << xin[i] << "  z=" << zout[i] << "\n";
+    }
+
+    return 0;
+}
